a1c4fbc6a7620dbb62966c01ec1110ef50a93e35
Raymond Feng <fengray@seas.upenn.edu>
2023-11-07 02:11:49 -0500

    basic layout

---
+++ familyhub/src/app/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return (
+        <div className="fixed left-0 top-0 flex w-full justify-center bg-amber-200 border-b py-8">
+        Home
+      </div>
+    )
+}
+
+export default Header
---
+++ familyhub/src/app/components/navigation.js
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+
+const Navigation = () => {
+
+    return (
+      <div className="bottom-0 fixed flex py-4 w-11/12 items-center justify-center bg-gray-100 rounded-full mb-4 shadow-lg">
+        <ul className="flex flex-row gap-10">
+          <li className="hover:bg-gray-200 rounded-md px-2 py-2"><a href="">
+            <Image
+              src="/home.svg"
+              alt="home"
+              width={28}
+              height={28}
+              priority
+            />
+            </a></li>
+            <li className="hover:bg-gray-200 rounded-md px-2 py-2"><a href="">
+            <Image
+              src="/calendar.svg"
+              alt="home"
+              width={32}
+              height={32}
+              priority
+            />
+            </a></li>
+            <li className="hover:bg-gray-200 rounded-md px-2 py-2"><a href="">
+            <Image
+              src="/checkin.svg"
+              alt="home"
+              width={32}
+              height={32}
+              priority
+            />
+            </a></li>
+            <li className="hover:bg-gray-200 rounded-md px-2 py-2"><a href="">
+            <Image
+              src="/memories.svg"
+              alt="home"
+              width={36}
+              height={36}
+              priority
+            />
+            </a></li>
+        </ul>
+      </div>
+    )
+}
+
+export default Navigation
