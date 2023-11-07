452d8e0181f3b64688caa86b2bee64502b63d90c
sshi02 <stevenlshi02@gmail.com>
2023-11-07 15:18:19 -0500

    added phone frame to desktop view

--- familyhub/src/app/components/header.js
+++ familyhub/src/app/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
 
     return (
-        <div className="fixed left-0 top-0 flex w-full justify-center bg-amber-200 border-b py-8">
+        <div className="flex w-full justify-center bg-amber-200 border-b py-8">
         Home
       </div>
     )
