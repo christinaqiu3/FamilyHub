43babd9074e76882592b0f958a1d1d943bce5e2b
wenxich <wenxi.c.chen@gmail.com>
2023-12-02 22:18:26 -0500

    memories: moved inline styling to stylesheet

--- familyhub/src/app/memories/page.jsx
+++ familyhub/src/app/memories/page.jsx
@@ -6,41 +6,18 @@ import "./memories.css"
 
 function Post() {
     return(
-        <div style={{
-            position: 'relative',
-            margin: '5px'
-        }}>
+        <div className="post-content">
             <img
                 src="https://64.media.tumblr.com/bff48b81714434830009b585a56bcfb6/5a69653ddc705238-72/s640x960/5c68eda0ec89ab529735e1f0d3eb6d167ef3ad1b.png"
                 alt="post"
-                style={{
-                    width: 150,
-                    height: 'auto',
-                    borderRadius: 10
-                }}
+                className="post-image"
             />
             <img
                 src="https://pbs.twimg.com/media/C4ORj7hUMAAK8sA.jpg"
                 alt="reigen"
-                style={{
-                    width: 50,
-                    height: 50,
-                    position: 'absolute',
-                    top: 2,
-                    left: 2,
-                    borderRadius: 100,
-                    border: '5px solid pink'
-                }}
+                className="poster-pfp"
             />
-        <p style={{
-            backgroundColor: 'white',
-            color: 'black',
-            position: 'absolute',
-            top: 10,
-            left: 60,
-            padding: '0px 5px',
-            borderRadius: 20
-        }}>
+        <p className="caption">
             caption
         </p>
         </div>
@@ -51,28 +28,11 @@ export default function Page() {
     return (
         <main>
             <Header title = "Memories"/>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                margin: '10px'
-            }}>
-                <div style={{
-                    borderRadius: 100,
-                    border: '1px solid black',
-                    padding: '10px',
-                    width: 40,
-                    height: 40,
-                }}>
+            <div id="options-bar">
+                <div id="upload-btn">
                     <Image src='/upload.svg' alt='upload' width={20} height={20} priority/>
                 </div>
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    border: '1px solid black',
-                    width: '220px',
-                    borderRadius: 20
-                }}>
+                <div id="search-bar">
                     <div style={{
                         margin: '0px 7px'
                     }}>
@@ -80,24 +40,8 @@ export default function Page() {
                     </div>
                     Search
                 </div>
-                <div style={{
-                    borderRadius: 100,
-                    border: '1px solid black',
-                    padding: '6px',
-                    width: 40,
-                    height: 40,
-                }}>
-                    <Image src='/filter.svg' alt='filter' width={29} height={29} priority/>
-                </div>
             </div>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                maxHeight: '70%',
-                overflow: 'scroll',
-                justifyContent: 'center'
-            }}>
+            <div id="posts">
                 <Post />
                 <Post />
                 <Post />
