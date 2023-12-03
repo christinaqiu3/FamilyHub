b3810c17e7a9cf34e435b01974d958d08800e14d
wenxich <wenxi.c.chen@gmail.com>
2023-12-02 22:23:15 -0500

    removed unnecessary imports from cal

--- familyhub/src/app/calendar/page.jsx
+++ familyhub/src/app/calendar/page.jsx
@@ -1,9 +1,6 @@
 "use client";
 import Navigation from '../components/navigation'
 import Header from '../components/header'
-import { DeviceFrameset } from 'react-device-frameset'
-import {isBrowser} from 'react-device-detect';
-import 'react-device-frameset/styles/marvel-devices.min.css'
 import '../globals.css'
 import Image from "next/image";
 import './calendar.css'
