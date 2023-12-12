1de8f046580a5fcfd0ba9371cd2ba3365307ea6c
wenxich <wenxi.c.chen@gmail.com>
2023-12-12 03:40:40 -0500

    deleted unnecessary code

--- familyhub/src/app/settings/page.jsx
+++ familyhub/src/app/settings/page.jsx
@@ -308,7 +308,7 @@ export default function SettingsPage() {
     function ColorButton({colorStyle, colorLightString, colorMedString, colorDarkString}) {
         return (
             <button className='color-button drop-shadow-md' style={colorStyle}
-                    onClick={() => setColor({colorLightString, colorMedString, colorDarkString})}></button>
+    onClick={() => setColor({colorLightString, colorMedString, colorDarkString})}/>
         )
     }
 
@@ -448,32 +448,6 @@ export default function SettingsPage() {
 
                     </div>
                 )}
-                {/*{avatarActive && (*/}
-                {/*    <div className='bg-gray-100 rounded-lg drop-shadow-xl absolute mt-44 z-20 p-4 ml-4 items-center flex flex-col gap-4'>*/}
-                {/*        <div className='flex flex-row justify-between w-full'>*/}
-                {/*            <div>Change icon</div>*/}
-                {/*            <button onClick={() => setAvatarActive(false)}>*/}
-                {/*                <svg fill="#000000" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 460.775 460.775">*/}
-                {/*                    <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55*/}
-                {/*                        c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55*/}
-                {/*                        c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505*/}
-                {/*                        c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55*/}
-                {/*                        l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719*/}
-                {/*                        c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>*/}
-                {/*                </svg>*/}
-                {/*            </button>*/}
-                {/*        </div>*/}
-                {/*        <input*/}
-                {/*            type="file"*/}
-                {/*            name="myImage"*/}
-                {/*            ref={hiddenFileInput}*/}
-                {/*            onChange={(event) => {*/}
-                {/*                setSelectedImage(event.target.files[0]);*/}
-                {/*            }}*/}
-                {/*            // style={{display: 'none'}}*/}
-                {/*        />*/}
-                {/*    </div>*/}
-                {/*)}*/}
                 {(colorActive || borderActive) && (
                     <div className="absolute w-full bg-gray-900" style={{
                         height: 602,
